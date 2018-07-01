878cd70617d2ec6a2c6425c839902773a08338b1
NelsonTJ <lixiaojun@tongji.edu.cn>
2018-07-02 01:29:44 +0800

    Improving project info window

--- iS3-Config/App.xaml.cs
+++ iS3-Config/App.xaml.cs
@@ -1,4 +1,37 @@
-﻿using System;
+﻿//************************  Notice  **********************************
+//** This file is part of iS3
+//**
+//** Copyright (c) 2018 Tongji University iS3 Team. All rights reserved.
+//**
+//** This library is free software; you can redistribute it and/or
+//** modify it under the terms of the GNU Lesser General Public
+//** License as published by the Free Software Foundation; either
+//** version 3 of the License, or (at your option) any later version.
+//**
+//** This library is distributed in the hope that it will be useful,
+//** but WITHOUT ANY WARRANTY; without even the implied warranty of
+//** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//** Lesser General Public License for more details.
+//**
+//** In addition, as a special exception,  that plugins developed for iS3,
+//** are allowed to remain closed sourced and can be distributed under any license .
+//** These rights are included in the file LGPL_EXCEPTION.txt in this package.
+//**
+//**************************************************************************
+
+//** This program is designed for configing data files for iS3.Desktop (standalone version).
+//** The data files usually reside in the iS3-Desktop-Stand-alone\Output\Data directory,
+//** including:
+//**          ProjectList.xml
+//**          <ProjectName>.xml
+//**          <ProjectName>\*.*
+//** This program depends on the following library:
+//**          .NET Framework 4.5
+//**          ArcGIS Runtime SDK for .NET 10.2.5
+//**          iS3.Core
+//
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -49,6 +82,8 @@ namespace iS3.Config
         {
             bool? success;
 
+            // Step 1 - Config path to iS3 and data directory
+            //
             ConfPathWindow mainWnd = new ConfPathWindow();
             success = mainWnd.ShowDialog();
             if (success == null || success.Value == false)
@@ -58,7 +93,10 @@ namespace iS3.Config
             iS3Path = mainWnd.ExePath;
             dataPath = mainWnd.DataPath;
 
-            ProjInfoWindow infoWnd = new ProjInfoWindow();
+            // Step 2 - Config project title and location information
+            //
+            string projListFile = dataPath + "\\ProjectList.xml";
+            ProjInfoWindow infoWnd = new ProjInfoWindow(projListFile);
             success = infoWnd.ShowDialog();
             if (success == null || success.Value == false)
             {
--- iS3-Config/ProjInfoWindow.xaml.cs
+++ iS3-Config/ProjInfoWindow.xaml.cs
@@ -12,9 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.IO;
+using System.Windows.Markup;
+using System.Xml.Serialization;
 using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.Layers;
 using Esri.ArcGISRuntime.Geometry;
+using IS3.Core;
 
 namespace iS3.Config
 {
@@ -27,15 +31,28 @@ namespace iS3.Config
         public double ProjLocX = 0;
         public double ProjLocY = 0;
 
+        string _projListFile;
+        ProjectList _projList;
+        GraphicsLayer _gLayer;
         PictureMarkerSymbol _pinMarkerSymbol = new PictureMarkerSymbol();
 
-        public ProjInfoWindow()
+        public ProjInfoWindow(string projListFile)
         {
             InitializeComponent();
 
-            StepLB.SelectedIndex = 0;
+            _projListFile = projListFile;
+
             InitializePictureMarkerSymbol();
+            MyMapView.Loaded += MyMapView_Loaded;
             MyMapView.MouseDown += MyMapView_MouseDown;
+
+            LoadProjectList();
+            if (_projList == null)
+                return;
+            foreach (ProjectLocation loc in _projList.Locations)
+            {
+                ProjectListLB.Items.Add(loc);
+            }
         }
 
         private async void InitializePictureMarkerSymbol()
@@ -51,58 +68,177 @@ namespace iS3.Config
             }
         }
 
-        private void StepLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        void LoadProjectList()
         {
-            int step = StepLB.SelectedIndex;
-            if (step == 0)
+            try
             {
-                prompt.Text = "Input project title in the title text box.";
-                ProjectTitleTB.Visibility = System.Windows.Visibility.Visible;
+                StreamReader reader = new StreamReader(_projListFile);
+                object obj = XamlReader.Load(reader.BaseStream);
+                _projList = obj as ProjectList;
             }
-            else if (step == 1)
+            catch (Exception error)
             {
-                prompt.Text = "Drop the project location on the map.";
-                ProjectTitleTB.Visibility = System.Windows.Visibility.Hidden;
+                MessageBox.Show(error.Message, "Error", MessageBoxButton.OK);
             }
         }
 
-        void MyMapView_MouseDown(object sender, MouseButtonEventArgs e)
+        void WriteProjectList()
         {
-            int step = StepLB.SelectedIndex;
-            if (step == 0)
+            try
+            {
+                //Stream writer = new FileStream(_projListFile + "1", FileMode.Create);
+                //XmlSerializer s = new XmlSerializer(_projList.GetType());
+                //s.Serialize(writer, _projList);
+                //writer.Close();
+
+                StreamWriter writer = new StreamWriter(_projListFile + "1");
+
+                string s = "";
+                foreach (ProjectLocation loc in _projList.Locations)
+                    s += XamlWriter.Save(loc) + "\n";
+
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void MyMapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _gLayer = Map.Layers["ProjectGraphicsLayer"] as GraphicsLayer;
+            if (_projList == null)
                 return;
+            foreach (ProjectLocation loc in _projList.Locations)
+            {
+                AddProjectLocationOnMap(loc);
+            }
+        }
 
-            GraphicsLayer gLayer = Map.Layers["ProjectGraphicsLayer"] as GraphicsLayer;
-            gLayer.Graphics.Clear();
-
-            Point screenPoint = e.GetPosition(MyMapView);
-            MapPoint loc = MyMapView.ScreenToLocation(screenPoint);
+        void AddProjectLocationOnMap(ProjectLocation loc)
+        {
             Graphic g = new Graphic()
             {
                 Geometry = new MapPoint(loc.X, loc.Y),
                 Symbol = _pinMarkerSymbol,
             };
-            gLayer.Graphics.Add(g);
+            g.Attributes["ID"] = loc.ID;
+            g.Attributes["DefinitionFile"] = loc.DefinitionFile;
+            g.Attributes["Description"] = loc.Description;
 
+            _gLayer.Graphics.Add(g);
+        }
+
+        private void ProjectListLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ProjectLocation loc = ProjectListLB.SelectedItem as ProjectLocation;
+            if (loc == null)
+            {
+                ProjectTitleTB.Visibility = Visibility.Hidden;
+                _gLayer.Graphics.Clear();
+                return;
+            }
+
+            ProjectTitleTB.Visibility = Visibility.Visible;
+            ProjectTitleTB.Text = loc.Description;
+            ProjectTitleTB.IsReadOnly = true;
+
+            _gLayer.Graphics.Clear();
+            AddProjectLocationOnMap(loc);
+
+            PromptTB.Text = "Select operations from the configuration.";
+            StepLB.SelectedIndex = -1;
+        }
+
+        private void StepLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ProjectLocation loc = ProjectListLB.SelectedItem as ProjectLocation;
+            if (loc == null)
+                return;
+
+            int step = StepLB.SelectedIndex;
+            if (step == 0)
+            {
+                PromptTB.Text = "Input project title in the title text box.";
+                ProjectTitleTB.IsReadOnly = false;
+                ProjectTitleTB.Focus();
+                ProjectTitleTB.SelectAll();
+                ProjectTitleTB.Visibility = System.Windows.Visibility.Visible;
+                _gLayer.Graphics.Clear();
+            }
+            else if (step == 1)
+            {
+                PromptTB.Text = "Drop the project location on the map.";
+                ProjectTitleTB.Visibility = System.Windows.Visibility.Hidden;
+                AddProjectLocationOnMap(loc);
+            }
+        }
+
+        private void ProjectTitleTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ProjectLocation loc = ProjectListLB.SelectedItem as ProjectLocation;
+            if (loc == null)
+                return;
+
+            loc.Description = ProjectTitleTB.Text;
+        }
+
+        void MyMapView_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ProjectLocation loc = ProjectListLB.SelectedItem as ProjectLocation;
+            if (loc == null)
+                return;
+
+            int step = StepLB.SelectedIndex;
+            if (step != 1)
+                return;
+
+            Point screenPoint = e.GetPosition(MyMapView);
+            MapPoint coord = MyMapView.ScreenToLocation(screenPoint);
+            _gLayer.Graphics.Clear();
+
+            loc.X = coord.X;
+            loc.Y = coord.Y;
+            AddProjectLocationOnMap(loc);
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            AddProjWindow addProjWnd = new AddProjWindow();
+            addProjWnd.Owner = this;
+            addProjWnd.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            bool? ok = addProjWnd.ShowDialog();
+            if (ok!=null && ok.Value==true)
+            {
+                ProjectLocation loc = new ProjectLocation();
+                loc.ID = addProjWnd.IdTB.Text;
+                loc.Description = addProjWnd.DescTB.Text;
+                loc.DefinitionFile = loc.ID + ".xml";
+                loc.X = 0;
+                loc.Y = 0;
+                _projList.Locations.Add(loc);
+
+                ProjectListLB.Items.Add(loc);
+            }
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            ProjectLocation loc = ProjectListLB.SelectedItem as ProjectLocation;
+            if (loc == null)
+                return;
+
+            _projList.Locations.Remove(loc);
+            ProjectListLB.Items.Remove(loc);
+
+            ProjectListLB.SelectedIndex = -1;
         }
 
         private void Next_Click(object sender, RoutedEventArgs e)
         {
+            WriteProjectList();
             // finish 
-            ProjName = ProjectTitleTB.Text;
-
-            GraphicsLayer gLayer = Map.Layers["ProjectGraphicsLayer"] as GraphicsLayer;
-            Graphic g = gLayer.Graphics.FirstOrDefault();
-            if (g != null)
-            {
-                MapPoint loc = g.Geometry as MapPoint;
-                if (loc != null)
-                {
-                    ProjLocX = loc.X;
-                    ProjLocY = loc.Y;
-                }
-            }
-
             DialogResult = true;
             Close();
         }
